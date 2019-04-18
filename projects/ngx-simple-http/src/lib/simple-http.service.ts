1fd619b26f25515c7ac52c64ce9f307d2009b167
Edric Chan <edric.chan.1997@gmail.com>
2019-04-18 22:50:48 +0800

    refactor: use `send` as prefix for all methods instead of `create`
    The old methods will be removed in a future release

--- projects/ngx-simple-http/src/lib/simple-http.service.ts
+++ projects/ngx-simple-http/src/lib/simple-http.service.ts
@@ -14,8 +14,21 @@ export class SimpleHttpService {
    * @template P A TypeScript interface to type the HTTP request's parameters to
    * @template R A TypeScript interface to type the HTTP request's response to
    * @return A `HttpClient` instance
+   * @deprecated Use {@link SimpleHttpService#sendHttpDelete}
    */
   createHttpDelete<P extends any, R extends any>(apiEndpoint: string, apiParams: P, apiHeaders: HttpHeaders = null): Observable<R> {
+    return this.sendHttpDelete<P, R>(apiEndpoint, apiParams, apiHeaders);
+  }
+  /**
+   * Constructs and sends a HTTP DELETE request
+   * @param apiEndpoint The API endpoint
+   * @param apiParams Parameters to append to the API endpoint
+   * @param apiHeaders HTTP headers to append to the request
+   * @template P A TypeScript interface to type the HTTP request's parameters to
+   * @template R A TypeScript interface to type the HTTP request's response to
+   * @return A `HttpClient` instance
+   */
+  sendHttpDelete<P extends any, R extends any>(apiEndpoint: string, apiParams: P, apiHeaders: HttpHeaders = null): Observable<R> {
     let params = new HttpParams();
     for (const prop in apiParams) {
       if (prop in apiParams && typeof apiParams[prop] !== undefined && apiParams[prop] !== null) {
@@ -39,6 +52,20 @@ export class SimpleHttpService {
       );
     }
   }
+
+  /**
+   * Constructs and sends a HTTP GET request
+   * @param apiEndpoint The API endpoint
+   * @param apiParams Parameters to append to the API endpoint
+   * @param apiHeaders HTTP headers to append to the request
+   * @template P A TypeScript interface to type the HTTP request's parameters to
+   * @template R A TypeScript interface to type the HTTP request's response to
+   * @return A `HttpClient` instance
+   * @deprecated Use {@link SimpleHttpService#sendHttpGet}
+   */
+  createHttpGet<P extends any, R extends any>(apiEndpoint: string, apiParams: P, apiHeaders: HttpHeaders = null): Observable<R> {
+    return this.sendHttpGet<P, R>(apiEndpoint, apiParams, apiHeaders);
+  }
   /**
    * Constructs and sends a HTTP GET request
    * @param apiEndpoint The API endpoint
@@ -48,7 +75,7 @@ export class SimpleHttpService {
    * @template R A TypeScript interface to type the HTTP request's response to
    * @return A `HttpClient` instance
    */
-  createHttpGet<P extends any, R extends any>(apiEndpoint: string, apiParams: P, apiHeaders: HttpHeaders = null): Observable<R> {
+  sendHttpGet<P extends any, R extends any>(apiEndpoint: string, apiParams: P, apiHeaders: HttpHeaders = null): Observable<R> {
     let params = new HttpParams();
     for (const prop in apiParams) {
       if (prop in apiParams && typeof apiParams[prop] !== undefined && apiParams[prop] !== null) {
@@ -71,6 +98,25 @@ export class SimpleHttpService {
         }
       );
     }
+  }
+
+  /**
+   * Constructs and sends a HTTP POST request
+   * @param apiEndpoint The API endpoint
+   * @param apiParams Parameters to append to the API endpoint
+   * @param apiBody The request body
+   * @param apiHeaders HTTP headers to append to the request
+   * @template B A TypeScript interface to type the HTTP request's body to
+   * @template P A TypeScript interface to type the HTTP request's parameters to
+   * @template R A TypeScript interface to type the HTTP request's response to
+   * @return A `HttpClient` instance
+   * @deprecated Use {@link SimpleHttpService#sendHttpPost}
+   */
+  createHttpPost<
+    B extends any,
+    P extends any,
+    R extends any>(apiEndpoint: string, apiParams: P, apiBody: B = null, apiHeaders: HttpHeaders = null): Observable<R> {
+    return this.sendHttpPost<B, P, R>(apiEndpoint, apiParams, apiBody, apiHeaders);
   }
   /**
    * Constructs and sends a HTTP POST request
@@ -83,7 +129,7 @@ export class SimpleHttpService {
    * @template R A TypeScript interface to type the HTTP request's response to
    * @return A `HttpClient` instance
    */
-  createHttpPost<
+  sendHttpPost<
     B extends any,
     P extends any,
     R extends any>(apiEndpoint: string, apiParams: P, apiBody: B = null, apiHeaders: HttpHeaders = null): Observable<R> {
@@ -144,6 +190,27 @@ export class SimpleHttpService {
    * @return A `HttpClient` instance
    */
   createHttpPut<
+    B extends any,
+    P extends any,
+    R extends any>(
+      apiEndpoint: string,
+      apiParams: P,
+      apiBody: B,
+      apiHeaders: HttpHeaders = null): Observable<R> {
+    return this.sendHttpPut<B, P, R>(apiEndpoint, apiParams, apiBody, apiHeaders);
+  }
+  /**
+   * Constructs and sends a HTTP PUT request
+   * @param apiEndpoint The API endpoint
+   * @param apiParams Parameters to append to the API endpoint
+   * @param apiBody The request body
+   * @param apiHeaders HTTP headers to append to the request
+   * @template B A TypeScript interface to type the HTTP request's body to
+   * @template P A TypeScript interface to type the HTTP request's parameters to
+   * @template R A TypeScript interface to type the HTTP request's response to
+   * @return A `HttpClient` instance
+   */
+  sendHttpPut<
     B extends any,
     P extends any,
     R extends any>(
